e18223596a347ca9b9e20d4759402a03ece53102
Jon Agüera <jonaguera@gmail.com>
2013-03-02 13:11:48 +0100

    Fin del ejercicios ListView

--- src/com/jonaguera/asteroides/Asteroides.java
+++ src/com/jonaguera/asteroides/Asteroides.java
@@ -4,57 +4,81 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Asteroides extends Activity {
 	private Button bAcercaDe;
-	private Button bSalir;
+	private Button bPuntuaciones;
+	private Button bPreferencias;
+	public static AlmacenPuntuaciones almacen = new AlmacenPuntuacionesArray();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		bAcercaDe = (Button) findViewById(R.id.button3);
-
+		bAcercaDe = (Button) findViewById(R.id.about);
 		bAcercaDe.setOnClickListener(new OnClickListener() {
-
 			public void onClick(View view) {
-
 				lanzarAcercaDe(null);
-
 			}
-
 		});
-		bSalir =(Button) findViewById(R.id.button4);
 
-		bSalir.setOnClickListener(new OnClickListener() {
-
-                   public void onClick(View view) {
-
-                        salir(null);
-
-                  }
-
-            });		
+		bPreferencias = (Button) findViewById(R.id.config);
+		bPreferencias.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
+				lanzarPreferencias(null);
+			}
+		});
+		bPuntuaciones = (Button) findViewById(R.id.records);
+		
+		bPuntuaciones.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
+				lanzarPuntuaciones(view);
+			}
+		});
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.acercaDe:
+			lanzarAcercaDe(null);
+			break;
+		case R.id.config:
+			lanzarPreferencias(null);
+			break;
+		}
 		return true;
 	}
 
 	public void lanzarAcercaDe(View view) {
-
 		Intent i = new Intent(this, AcercaDe.class);
-
 		startActivity(i);
-
 	}
-	public void salir(View view){
+
+	public void lanzarPreferencias(View view) {
+		Intent i = new Intent(this, Preferencias.class);
+		startActivity(i);
+	}
+
+	public void lanzarPuntuaciones(View view) {
+		Intent i = new Intent(this, Puntuaciones.class);
+		startActivity(i);
+	}
+
+	public void salir(View view) {
 		finish();
 
 	}
